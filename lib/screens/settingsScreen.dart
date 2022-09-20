363429d0163f77067e6a321cd242131795d01c00
mvharikrishnan <harikrish.hmv@gmail.com>
2022-09-20 15:54:02 +0530

    Settings

--- lib/screens/settingsScreen.dart
+++ lib/screens/settingsScreen.dart
@@ -16,17 +16,158 @@ class SettingsScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.only(left: 15, right: 15),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                'Settings',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 36,
-                  fontWeight: FontWeight.w500,
-                ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Column(
+                    //for giving space btw icons and version
+
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Notification',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Terms And Condition',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Privacy Policy',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Theme',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SizedBox(height: 15,),
-              
+              Column(
+                children: [
+                  Text(
+                    'Connect With Us',
+                    style: TextStyle(color: Color(0xFFD933C3), fontSize: 20),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.facebook_outlined,
+                          size: 48,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.email,
+                          size: 48,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        'App Version',
+                        style:
+                            TextStyle(color: Color(0xFFD594EE), fontSize: 15),
+                      ),
+                      Text(
+                        '1.0.0',
+                        style:
+                            TextStyle(color: Color(0xFFD594EE), fontSize: 15),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ],
           ),
         ),
